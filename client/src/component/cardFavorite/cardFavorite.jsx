1916aceb32368bf11df315a2fe0359b6e3417a9a
fermin234 <fermin234@hotmail.com>
2022-11-13 15:48:27 -0300

    Funcionalidad favoritos terminada

---
+++ client/src/component/cardFavorite/cardFavorite.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import s from './cardFavorite.module.css'
+
+export default function CardFavorite({ name, genres, image, id, setUpdate, update }) {
+
+  function handelDeleted() {
+    localStorage.removeItem(id)
+    setUpdate(!update)
+    console.log(id);
+  }
+  return (
+    <div>
+      <div className={s.containerFav}>
+        <button className={s.fav} onClick={() => handelDeleted()}> X </button>
+      </div>
+      <Link className={s.div} to={`/detail/${id}`} >
+        <img className={s.img} src={image} alt={`${name}-imagen`} />
+        <h3>{name}</h3>
+        <h6>{genres.map(e => ` ${e} `)}</h6>
+      </Link>
+    </div>
+  )
+}
