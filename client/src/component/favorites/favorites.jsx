b8c628793c88df9e788a63156723b8a539c5a213
fermin234 <fermin234@hotmail.com>
2022-11-12 21:32:17 -0300

    Se agrega funcionalidad de favoritos

---
+++ client/src/component/favorites/favorites.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import Card from "../card/card";
+import NavBar from "../navBar/navBar";
+import s from './favorites.module.css'
+
+export default function Favorites({ match }) {
+
+  const [favs, setFavs] = useState([])
+
+  const dispatch = useDispatch()
+  for (const key in localStorage) {
+    favs.push(JSON.parse(localStorage.getItem(key)))
+  }
+
+
+  function removeFav() {
+    localStorage.clear()
+    setFavs([])
+  }
+
+  return (
+    <>
+      <NavBar match={match} />
+      <div className={s.containerCardsFav}>
+        {favs?.map(e => e !== null ? <Card key={e.id} name={e.name} genres={e.genres} image={e.image} /> : null)}
+      </div>
+      <button onClick={() => removeFav()}>Eliminar todos los favoritos</button>
+    </>
+  )
+}
