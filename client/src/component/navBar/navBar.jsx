bfe22eb49dea0ca7867892001b46623b2e239703
fermin234 <fermin234@hotmail.com>
2023-01-17 23:38:01 -0300

    Add buttons navbar

--- client/src/component/navBar/navBar.jsx
+++ client/src/component/navBar/navBar.jsx
@@ -2,10 +2,7 @@ import React from "react";
 import s from './navBar.module.css'
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from "react-redux";
-import { VideoGameByName, desplegarFiltros } from "../../redux/actions";
-
-import img from './img/4.png'
-import Filtered from "../filtered/filtered";
+import { VideoGameByName } from "../../redux/actions";
 
 export default function NavBar({ match, setCurrentPage }) {
 
@@ -27,41 +24,26 @@ export default function NavBar({ match, setCurrentPage }) {
     e.preventDefault()
     form.reset()
   }
+  console.log();
   return (
     <div className={s.containerAll} >
-      {/* <div className={s.div}>
-        {match.url === "/home" ?
-          <div className={s.containerSearch}>
-            <svg className={s.loupin} xmlns="http://www.w3.org/2000/svg"  >
-              <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" />
-            </svg>
-            <form id="form" className={s.form} onSubmit={onHandelSubmit}>
-              <input className={s.input} name='input' type="search" placeholder="Search Video Game" autoComplete="off" onChange={onHandleChange} />
-            </form>
-          </div>
-          : null}
-
-        {match.url === "/home" ?
-          <div className={s.divLeftNavBar}>
-            <Link to='/create'>
-              <button> Create </button>
-            </Link>
-            <Link to='/favorites'>
-              <button> Favorites </button>
-            </Link>
-          </div>
-          : null}
-      </div> */}
       <div className={s.containerFilter}>
-        <Link className={s.link} to="/home">
-          <label className={s.label}>{"<=="}</label>
-        </Link>
-        <h1>Filters</h1>
+        {match.path !== "/home" &&
+          <Link className={s.link} to="/home">
+            <label className={s.label}>
+              <svg height="16" width="16" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 1024 1024"><path d="M874.690416 495.52477c0 11.2973-9.168824 20.466124-20.466124 20.466124l-604.773963 0 188.083679 188.083679c7.992021 7.992021 7.992021 20.947078 0 28.939099-4.001127 3.990894-9.240455 5.996574-14.46955 5.996574-5.239328 0-10.478655-1.995447-14.479783-5.996574l-223.00912-223.00912c-3.837398-3.837398-5.996574-9.046027-5.996574-14.46955 0-5.433756 2.159176-10.632151 5.996574-14.46955l223.019353-223.029586c7.992021-7.992021 20.957311-7.992021 28.949332 0 7.992021 8.002254 7.992021 20.957311 0 28.949332l-188.073446 188.073446 604.753497 0C865.521592 475.058646 874.690416 484.217237 874.690416 495.52477z"></path></svg></label>
+          </Link>
+        }
+        {match.path === "/home" &&
+          <h1>Filters</h1>
+        }
+        {match.path === "/favorites" &&
+          <h1>List</h1>
+        }
       </div>
       <div className={s.containterNavBar}>
         <Link className={s.link} to='/create'>
           <label className={s.label}>Create</label>
-          {/* <button> Create </button> */}
         </Link>
         <form id="form" className={s.form} onSubmit={onHandelSubmit}>
           <input className={s.input} name='input' type="search" placeholder="Search Video Game" autoComplete="off" onChange={onHandleChange} />
