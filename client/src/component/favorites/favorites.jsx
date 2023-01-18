cdc63c05775c733e65a41613722f0e50ee7dba89
fermin234 <fermin234@hotmail.com>
2023-01-17 23:37:48 -0300

    Changes favorites

--- client/src/component/favorites/favorites.jsx
+++ client/src/component/favorites/favorites.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import Card from "../card/card";
 import CardFavorite from "../cardFavorite/cardFavorite";
 import NavBar from "../navBar/navBar";
 import s from './favorites.module.css'
@@ -9,11 +10,16 @@ export default function Favorites({ match }) {
   let [update, setUpdate] = useState(false)
 
   return (
-    <>
+    <div>
       <NavBar match={match} />
-      <div className={s.containerCardsFav}>
-        {JSON.parse(localStorage.getItem("favorites"))?.map(e => <CardFavorite key={e.id} name={e.name} genres={e.genres} image={e.image} id={e.id} setUpdate={setUpdate} update={update} />)}
+      <div className={s.container}>
+        <div className={s.containerList}>
+
+        </div>
+        <div className={s.contarinerCard}>
+          {JSON.parse(localStorage.getItem("favorites"))?.map(e => <CardFavorite key={e.id} name={e.name} genres={e.genres} image={e.image} id={e.id} setUpdate={setUpdate} update={update} />)}
+        </div>
       </div>
-    </>
+    </div>
   )
 }
