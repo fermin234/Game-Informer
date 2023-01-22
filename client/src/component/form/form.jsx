d5096210cb5b4fe69bf979a2e20af81f6908df75
fermin234 <fermin234@hotmail.com>
2023-01-22 18:41:34 -0300

    feat form persistent

--- client/src/component/form/form.jsx
+++ client/src/component/form/form.jsx
@@ -1,26 +1,32 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { createVideoGame, ResetFilter, allVideoGames } from "../../redux/actions";
-import NavBar from "../navBar/navBar";
-import s from './form.module.css'
+import NavBar from "../NavBar/NavBar";
+import s from './Form.module.css'
 import Validate from "./validate.js";
-import * as allImages from '../../assets'
+import * as allImages from '../../assets/iconsGenres'
+import { useModal } from "../../hooks/useModal";
+import ModalInformation from "../Modals/ModalInformation";
 
 export default function Form(props) {
 
   const dispatch = useDispatch()
+  !localStorage.getItem("form") && localStorage.setItem("form", "{}")
+  const [form, setForm] = useState(JSON.parse(localStorage.getItem("form")))
 
   const [input, setInput] = useState({
-    name: "",
-    description: "",
-    released: "",
-    rating: null,
-    platforms: [],
-    genres: [],
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSch19yXTth6yL5J-SU6FafjJAUv1C1ptwziIyqk_3Skw&s.png",
-    created: true
+    name: form?.name ? form.name : "",
+    description: form?.description ? form.description : "",
+    released: form?.released ? form.released : "",
+    rating: form?.rating ? form.rating : null,
+    platforms: form?.platforms ? form.platforms : [],
+    genres: form?.genres?.length ? form.genres : [],
+    image: form?.image ? form.image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSch19yXTth6yL5J-SU6FafjJAUv1C1ptwziIyqk_3Skw&s.png",
+    created: form?.created ? form.created : true
   })
   const images = []
+  const [isOpen, openModal, closeModal] = useModal()
+
 
 
   for (const e in allImages) {
@@ -31,6 +37,9 @@ export default function Form(props) {
 
   function handleSubmit(e) {
     e.preventDefault()
+    if (Object.entries(errors).length) {
+      return openModal()
+    }
     dispatch(createVideoGame(input))
     dispatch(allVideoGames())
     dispatch(ResetFilter())
@@ -51,7 +60,6 @@ export default function Form(props) {
   }
 
   function onHandleChange(e) {
-    console.log(e.target.id);
     if (e.target.name === "platforms") {
       setInput({
         ...input,
@@ -59,14 +67,19 @@ export default function Form(props) {
       })
     } else if (e.target.name === "genres") {
       if (input.genres.includes(e.target.id)) {
+        console.log('entra al if')
         const id = e.target.id
         let arr = input.genres
         const index = arr.findIndex(e => e === id)
         arr.splice(index, 1)
+        // let form = JSON.parse(localStorage.getItem("form"))
+        // form.genres = arr
+        // localStorage.setItem("form", JSON.stringify(form))
         setInput({
           ...input,
           genres: arr
         })
+
       } else {
         setInput({
           ...input,
@@ -88,6 +101,7 @@ export default function Form(props) {
 
   useEffect(() => {
     setErrors(Validate(input))
+    localStorage.setItem("form", JSON.stringify(input))
   }, [input])
 
   function resetForm(e) {
@@ -103,43 +117,48 @@ export default function Form(props) {
     <>
       <NavBar match={props} />
       <div className={s.containerAll}>
+        <ModalInformation isOpen={isOpen} closeModal={closeModal}>
+          <h2>{Object.values(errors)[0]}</h2>
+        </ModalInformation>
         <form className={s.form} onSubmit={handleSubmit} method="post" id="form">
           {/* Name */}
           <label htmlFor="">*Name:</label>
-          <input type="text" name="name" autoComplete="off" placeholder="Counter Strike 1.6" onChange={onHandleChange} />
-          <p className={errors.name ? s.danger : s.succes}>Debe contener un nombre.</p>
-          <p className={errors.name ? s.danger : s.succes}>El nombre debe contener mas de una letra.</p>
+          <input type="text" name="name" autoComplete="off" placeholder="Counter Strike 1.6" onChange={onHandleChange}
+            value={input.name && input.name} />
+
           {/* Description */}
-          <label htmlFor="">*Description:</label>
-          <input type="text" name="description" autoComplete="off" placeholder="The shooter game..." onChange={onHandleChange} />
-          <p className={errors.description ? s.danger : s.succes}>Se debe ingresar una descripcion.</p>
+          <label htmlFor="" >* Description:</label>
+          <input type="text" name="description" autoComplete="off" placeholder="The shooter game..." onChange={onHandleChange}
+            value={input.description && input.description} />
+
           {/* Released */}
           <label htmlFor="">Released:</label>
-          <input type="date" name="released" autoComplete="off" onChange={onHandleChange} />
-          <p className={s.danger}>{errors && errors.released}</p>
+          <input type="date" name="released" autoComplete="off" onChange={onHandleChange} className={s.inputReleased}
+            value={input.released && input.released} />
+
           {/* Rating */}
           <label htmlFor="">*Rating:</label>
-          <input type="input" name="rating" autoComplete="off" placeholder="0-5" onChange={onHandleChange} />
-          <p className={errors.rating ? s.danger : s.succes}>El rating debe estar entre 0 y 5.</p>
+          <input type="input" name="rating" autoComplete="off" placeholder="0-5" onChange={onHandleChange}
+            value={input.rating && input.rating} />
+
           {/* Platforms */}
           <label htmlFor="">Platforms:</label>
-          <input type="text" name="platforms" autoComplete="off" placeholder="PC" onChange={onHandleChange} />
-          <p className={s.danger}>{errors && errors.platforms}</p>
-          {/* Genres */}
-          {/* <label htmlFor="">Genres:</label>
-          <input type="text" name="genres" autoComplete="off" placeholder="Shooter Action" onChange={onHandleChange} />
-          <p className={s.danger}>{errors && errors.genres}</p> */}
+          <input type="text" name="platforms" autoComplete="off" placeholder="PC" onChange={onHandleChange}
+            value={input.platforms && input.platforms} />
+
           {/* Image */}
           <label htmlFor="">Image:</label>
-          <input type="text" name="image" autoComplete="off" placeholder="URL" onChange={onHandleChange} />
-          <p className={s.danger}>{errors && errors.image}</p>
+          <input type="text" name="image" autoComplete="off" placeholder="URL" onChange={onHandleChange}
+            value={input.image !== "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSch19yXTth6yL5J-SU6FafjJAUv1C1ptwziIyqk_3Skw&s.png" ? input.image : ""} />
+
           {/* Create */}
-          <button type="submit" disabled={Object.entries(errors).length ? true : false}> Create </button>
+          <button type="submit" > Create </button>
         </form>
         <div className={s.divContarner}>
           <div>
             <h2>Select genres for your video game</h2>
           </div>
+          {/* Genres */}
           <div className={s.containgerGenres}>
             {
               images?.map(e => {
@@ -158,12 +177,6 @@ export default function Form(props) {
           <h3 className={s.name}>{input.name}</h3>
           <div className={s.labels}>{input.genres.map(e => <label key={Math.random() * 100}>{e}</label>)}
           </div>
-
-          {/* <img className={s.img} src={input.image} alt={`${input.name}-imagen`} />
-          <h1>{input.name}</h1>
-          <div className={s.containerLabels}>
-            {input.genres.map(e => <label>{e}</label>)}
-          </div> */}
         </div>
       </div>
     </>)
