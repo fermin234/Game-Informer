75c7cfadb72d7760f0f8e9bed8a8a47b0992fd98
fermin234 <fermin234@hotmail.com>
2023-01-26 14:52:00 -0300

    added screenshots pages

---
+++ client/src/assets/App/index.js
@@ -0,0 +1,8 @@
+export { default as CreateVideoGame } from "./CreateVideoGame.png";
+export { default as DeleteVideoGame } from "./DeleteVideoGame.png";
+export { default as Favorites } from "./Favorites.png";
+export { default as Favorites2 } from "./Favorites2.png";
+export { default as Home } from "./Home.png";
+export { default as VideoGameDetail1 } from "./VideoGameDetail1.png";
+export { default as VideoGameDetail2 } from "./VideoGameDetail2.png";
+export { default as VideoGameDetail3 } from "./VideoGameDetail3.png";
