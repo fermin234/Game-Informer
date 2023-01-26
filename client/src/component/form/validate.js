e2c03773d0a979e787f10298bc52e8d380289c86
fermin234 <fermin234@hotmail.com>
2023-01-26 14:54:37 -0300

    refactor function validate form

--- client/src/component/form/validate.js
+++ client/src/component/form/validate.js
@@ -1,9 +1,16 @@
 export default function Validate(input) {
   const errors = {};
-  if (input.name < 2) errors.name = 'El nombre debe contener mas de una letra.';
-  if (input.rating > 5 || input.rating < 0 || input.rating === null)
-    errors.rating = 'El rating debe estar entre 0 y 5.';
-  if (!input.description)
-    errors.description = 'Se debe ingresar una descripcion.';
+  if (input.name < 2)
+    errors.name = "The name must contain more than one letter.";
+  if (
+    input.rating > 5 ||
+    input.rating < 0 ||
+    input.rating === null ||
+    input.rating === ""
+  )
+    errors.rating = "The rating must be between 0 and 5.";
+  if (!input.description) errors.description = "A description must be entered.";
+  if (!input.genres.length)
+    errors.genres = "Choose at least one genre for the video game.";
   return errors;
 }
