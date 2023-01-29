17cf27b2fb4d256942068b380f7852d8b8ea1c60
fermin234 <fermin234@hotmail.com>
2023-01-29 02:00:03 -0300

    added icons & photos pages

--- client/src/assets/App/index.js
+++ client/src/assets/App/index.js
@@ -1,8 +1,12 @@
-export { default as CreateVideoGame } from "./CreateVideoGame.png";
-export { default as DeleteVideoGame } from "./DeleteVideoGame.png";
+export { default as LandingPage } from "./LandingPage.png";
+export { default as Home } from "./Home.png";
 export { default as Favorites } from "./Favorites.png";
 export { default as Favorites2 } from "./Favorites2.png";
-export { default as Home } from "./Home.png";
+export { default as Favorites3 } from "./Favorites3.png";
+export { default as Favorites4 } from "./Favorites4.png";
+export { default as Favorites5 } from "./Favorites5.png";
 export { default as VideoGameDetail1 } from "./VideoGameDetail1.png";
 export { default as VideoGameDetail2 } from "./VideoGameDetail2.png";
-export { default as VideoGameDetail3 } from "./VideoGameDetail3.png";
+export { default as CreateVideoGame } from "./CreateVideoGame.png";
+export { default as CreateVideoGame2 } from "./CreateVideoGame2.png";
+export { default as DeleteVideoGame } from "./DeleteVideoGame.png";
---
+++ client/src/assets/Socials/index.js
@@ -0,0 +1,5 @@
+export { default as github } from "../Socials/github.png";
+export { default as linkedin } from "../Socials/linkedin.png";
+export { default as portfolio } from "../Socials/portfolio.png";
+export { default as whatsapp } from "../Socials/whatsapp.png";
+export { default as diagonal } from "../Socials/diagonal.png";
