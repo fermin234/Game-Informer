e363905260172fa175a680551668eab4078101fe
fermin234 <fermin234@hotmail.com>
2023-01-30 02:48:05 -0300

    refactor landingPage

--- client/src/component/landingPage/landingPage.jsx
+++ client/src/component/landingPage/landingPage.jsx
@@ -1,14 +1,12 @@
 import React from "react";
-import s from './landingPage.module.css'
+import s from './LandingPage.module.css'
 import { Link } from 'react-router-dom'
-import img from './img/12.jpg'
 
 export default function LandingPage() {
   return (
     <div className={s.div}>
-      <img src={img} alt="imagen-de-carga" />
-      <Link to='/home'>
-        <button> Join the Game! </button>
+      <Link className={s.link} to='/home'>
+        <button className={s.button}> Join the Game! </button>
       </Link>
     </div>
   )
