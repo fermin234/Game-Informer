03708fcdc1ad1a04ce3a961b06a1239a6f3695e3
fermin234 <fermin234@hotmail.com>
2023-01-30 11:44:13 -0300

    fixed to deploy v6

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,14 @@
-import App from './App';
-import store from './redux/store';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import dotenv from 'dotenv';
-import axios from 'axios';
-dotenv.config();
+import App from "./App";
+import store from "./redux/store";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import axios from "axios";
 
-axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001';
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
 ReactDOM.render(
   <Provider store={store}>
@@ -18,7 +16,7 @@ ReactDOM.render(
       <App />
     </BrowserRouter>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
