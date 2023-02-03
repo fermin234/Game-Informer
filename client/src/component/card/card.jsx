a976fbb9e05d3a8b5eec332ef3b9ea149840d93d
fermin234 <fermin234@hotmail.com>
2023-02-03 12:01:49 -0300

    fixed delete v6

--- client/src/component/card/card.jsx
+++ client/src/component/card/card.jsx
@@ -34,11 +34,11 @@ export default function Card({ id, name, genres, image, platforms, created, matc
   }
 
   async function handleDelete(e) {
-    let result = false
-    result = await axios.get(`/validatePassword?password=${e.target.value}`)
-    if (result.data === true) {
-      dispatch(deleteVideoGame(id))
-    }
+    // let result = false
+    // result = await axios.get(`/validatePassword?password=${e.target.value}`)
+    // if (result.data === true) {
+    dispatch(deleteVideoGame(id))
+    // }
   }
 
   return (
